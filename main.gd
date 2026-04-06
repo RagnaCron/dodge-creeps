7c404343804b0db805df22bfb5dffd68fb20c9dc
Manuel Werder <manuelwerder@hotmail.com>
2026-04-06 16:16:56 +0200

    Adding Background color and music for the finishing touch

--- main.gd
+++ main.gd
@@ -12,12 +12,15 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 
 func _on_mob_timer_timeout() -> void:
